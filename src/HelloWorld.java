b6da076d0726e8384072866c4a832dadf7a1df13
André Santos <asantos@fosters.vi.pt>
2016-02-05 18:07:12 +0000

    mais prints

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,26 @@ public class HelloWorld
 	public static void main (String [] args)
 	{
 		System.out.println("hello world again");
+		System.out.println("hello world again");
+		System.out.println("hello world again");
+		System.out.println("hello world again");
+		System.out.println("hello world again");
+		System.out.println("hello world again");
+		System.out.println("hello world again");
+		System.out.println("hello world again");
+		System.out.println("hello world again");
+		System.out.println("hello world again");
+		System.out.println("hello world again");
+		System.out.println("hello world again");
+		System.out.println("hello world again");
+		System.out.println("hello world again");
+		System.out.println("hello world again");
+		System.out.println("hello world again");
+		System.out.println("hello world again");
+		System.out.println("hello world again");
+		System.out.println("hello world again");
+		System.out.println("hello world again");
+		System.out.println("hello world again");
+		System.out.println("hello world again");
 	}
 }
